a8ef1ab5aeaa3d0479f2274a812574c6211a6d6d
Hugo Gomes <hugorgomes10@gmail.com>
2022-10-12 09:37:33 -0300

    fixed erro index.tsx & Marvel API connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import md5 from 'md5';
 
+const publicKey = 'e0a7af9e09930a9a1ea31aded90ee216';
+const privateKey = 'c1878a0dd01fa9fdec33141b09818052b87cde30';
+
+const time = Number(new Date());
+
+const hash = md5(time + privateKey + publicKey);
+
 const App: React.FunctionComponent = () => {
-  return <div className="App">Marvel API</div>
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://gateway.marvel.com/v1/public/comics?ts=${time}&apikey=${publicKey}&hash=${hash}`,
+      )
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+  }, [])
+  return <div>Marvel API</div>
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+let root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-const root = ReactDOM.createRoot();
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+)
